97683358ce4e9b29924e804b5626f7ca0b7ec6ca
Kieran David Evans <keyz182@gmail.com>
2025-05-28 13:48:21 +0100

    Try patch hospitality for more work

---
+++ 1.5/Source/GDFP.Hospitality/GDFPHospitalityMod.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace GDFP.Hospitality;
+
+public class GDFPHospitalityMod : Mod
+{
+    public static Settings settings;
+
+    public GDFPHospitalityMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from GDFP.Hospitality");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("MSSG.FlavourPack.GDFP.Hospitality.main");
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "GDFP_Settings".Translate();
+    }
+}
