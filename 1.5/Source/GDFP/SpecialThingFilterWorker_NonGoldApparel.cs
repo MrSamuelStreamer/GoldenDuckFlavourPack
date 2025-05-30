1f64d57b31452380529de7fb37aa071b1f9339fa
Kieran David Evans <keyz182@gmail.com>
2025-05-30 11:07:20 +0100

    Apparel filter and frogge oasis

---
+++ 1.5/Source/GDFP/SpecialThingFilterWorker_NonGoldApparel.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace GDFP;
+
+public class SpecialThingFilterWorker_NonGoldApparel : SpecialThingFilterWorker
+{
+    public override bool Matches(Thing t) => t is Apparel apparel && (apparel.Stuff == null || apparel.Stuff != ThingDefOf.Gold);
+
+    public override bool CanEverMatch(ThingDef def)
+    {
+        return def.IsApparel && def.MadeFromStuff;
+    }
+
+}
