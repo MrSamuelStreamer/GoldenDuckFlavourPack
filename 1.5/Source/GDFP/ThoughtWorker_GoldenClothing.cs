043e89b815e192649b4a72cf92cde949edf0942f
Kieran David Evans <keyz182@gmail.com>
2025-05-30 10:19:47 +0100

    Golden clothing thoughts

---
+++ 1.5/Source/GDFP/ThoughtWorker_GoldenClothing.cs
@@ -0,0 +1,8 @@
+﻿using RimWorld;
+
+namespace GDFP;
+
+public class ThoughtWorker_GoldenClothing: ThoughtWorker_ApparelThought
+{
+    protected override bool ApparelCounts(Apparel apparel) => apparel.Stuff == ThingDefOf.Gold;
+}
