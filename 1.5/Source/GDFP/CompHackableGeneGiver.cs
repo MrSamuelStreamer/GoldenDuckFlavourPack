3725ded7d3e828c7de05fb5a9c329761db62e0df
Kieran David Evans <keyz182@gmail.com>
2025-06-02 21:50:58 +0100

    GeneRepo

---
+++ 1.5/Source/GDFP/CompHackableGeneGiver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace GDFP;
+
+public class CompHackableGeneGiver: ThingComp
+{
+    public CompProperties_HackableGeneGiver Props => (CompProperties_HackableGeneGiver)this.props;
+    public override void ReceiveCompSignal(string signal)
+    {
+        if(Props.GenePool.NullOrEmpty()) return;
+        if (signal != Props.Signal) return;
+
+        Pawn user = parent.Map.thingGrid.ThingAt<Pawn>(parent.InteractionCell);
+        if(user?.genes == null) return;
+
+        List<GeneDef> pool = Props.GenePool.ToList();
+        user.genes.GenesListForReading.ForEach(gene=>pool.Remove(gene.def));
+
+        if(pool.NullOrEmpty()) return;
+        GeneDef gene = pool.RandomElement();
+
+        user.genes.AddGene(gene, true);
+
+        Messages.Message("GDFP_GotNewGene".Translate(user.Named("PAWN"), gene.LabelCap), new LookTargets(user), MessageTypeDefOf.PositiveEvent);
+    }
+
+}
---
+++ 1.5/Source/GDFP/CompProperties_HackableGeneGiver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace GDFP;
+
+public class CompProperties_HackableGeneGiver: CompProperties
+{
+    public string Signal = "Hackend";
+    public List<GeneDef> GenePool;
+
+    public CompProperties_HackableGeneGiver()
+    {
+        compClass = typeof(CompHackableGeneGiver);
+    }
+}
