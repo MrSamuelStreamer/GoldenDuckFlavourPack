83973d5721bd5cedcef380198247da799f5b8560
Kieran David Evans <keyz182@gmail.com>
2025-06-10 09:05:13 +0100

    Auto add golems to faction as slaves

---
+++ 1.5/Source/GDFP/CompAddAsSlaveToPlayerFaction.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace GDFP;
+
+public class CompAddAsSlaveToPlayerFaction: ThingComp
+{
+    public override void PostSpawnSetup(bool respawningAfterLoad)
+    {
+        if (!parent.def.CanHaveFaction || parent is not Pawn pawn) return;
+
+        pawn.guest.SetGuestStatus(Faction.OfPlayer, GuestStatus.Slave);
+    }
+
+}
